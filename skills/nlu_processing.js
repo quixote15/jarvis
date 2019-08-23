3a0008223a4bea089e4ff02b2eca78c25b123175
quixote15 <santos2tiago@gmail.com>
2019-08-23 17:58:41 -0300

    adiciona delay no nlu

--- skills/nlu_processing.js
+++ skills/nlu_processing.js
@@ -1,30 +1,46 @@
-
 module.exports = function(controller) {
+  var rasa = require("./midleware-rasa")({
+    rasa_uri: "http://165.22.220.204:5005",
+    rasa_project: "jarvis"
+  });
+  controller.middleware.receive.use(rasa.receive);
 
-    var rasa = require('./midleware-rasa')({rasa_uri: 'http://localhost:5005',rasa_project: 'jarvis'});
-    controller.middleware.receive.use(rasa.receive);
-  
-    controller.on('message_received', function(bot, message) {
-    
-        console.log('mensagem saas:', message);
-        const { replies } = message;
-        //console.log(replies)
-        replies.forEach(answer => {
-            console.log('text:', answer.text || answer.image)
-           const {text, image} = answer;
-           const reply = {
-              text,
-              files: [
-                {
-                  url: image,
-                  image: !!image
-                }
-              ]
-            };
-            bot.reply(message,reply)
+  controller.on("message_received", async function(bot, message) {
+    console.log("mensagem saas:", message);
 
-        })
-        //bot.startConversation()
+    const { replies } = message;
+    //console.log(replies)
+ 
+    for (var i = 0; i < replies.length; i++) {
+      const answer = replies[i];
+      console.log("text:", answer.text || answer.image);
 
-    });
-}
+      const { text, image } = answer;
+      const reply = {
+        text,
+        replyWithTyping: true,
+        files: [
+          {
+            url: image,
+            image: !!image
+          }
+        ]
+      };
+      /* bot.reply(message, reply, function() {
+        console.log("done ", index);
+      });*/
+      await sendMessage(bot, message, reply);
+    }
+
+    /*replies.forEach(async (answer, index) => {
+      
+    });*/
+    //bot.startConversation()
+  });
+
+  const sendMessage = async (bot, message, reply) => {
+    return new Promise(resolve =>
+      setTimeout(() => resolve(bot.reply(message, reply)), 1000)
+    );
+  };
+};
