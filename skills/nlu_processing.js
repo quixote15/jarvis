f79a378f333cf79fdab782133c893664cd43d057
quixote15 <santos2tiago@gmail.com>
2019-09-05 13:51:29 -0300

    Revert "Adiciona theads pra corrigir dissincronia"
    This reverts commit 0dce168d3b125ab5caa3d32aa3897064266e114f.

--- skills/nlu_processing.js
+++ skills/nlu_processing.js
@@ -6,33 +6,41 @@ module.exports = function(controller) {
   controller.middleware.receive.use(rasa.receive);
 
   controller.on("message_received", async function(bot, message) {
-    //console.log("mensagem saas:", message);
+    console.log("mensagem saas:", message);
 
     const { replies } = message;
+    //console.log(replies)
+ 
+    for (var i = 0; i < replies.length; i++) {
+      const answer = replies[i];
+      console.log("text:", answer.text || answer.image);
 
-    bot.startConversation(message, function(err, convo) {
-      for (var i = 0; i < replies.length; i++) {
-        const answer = replies[i];
-      //  console.log("text:", answer.text || answer.image);
-      //  console.log("message:", message);
+      const { text, image } = answer;
+      const reply = {
+        text,
+        replyWithTyping: true,
+        files: [
+          {
+            url: image,
+            image: !!image
+          }
+        ]
+      };
+      /* bot.reply(message, reply, function() {
+        console.log("done ", index);
+      });*/
+      await sendMessage(bot, message, reply);
+    }
 
-        const { text, image } = answer;
-        const reply = {
-          text,
-          replyWithTyping: true,
-          files: [
-            {
-              url: image,
-              image: !!image
-            }
-          ]
-        };
-
-        convo.say(reply);
-
-      }
-    });
-    
+    /*replies.forEach(async (answer, index) => {
+      
+    });*/
+    //bot.startConversation()
   });
 
+  const sendMessage = async (bot, message, reply) => {
+    return new Promise(resolve =>
+      setTimeout(() => resolve(bot.reply(message, reply)), 1000)
+    );
+  };
 };
