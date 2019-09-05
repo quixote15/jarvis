50bcca8662f68dfe56dd4acff770df40490cdad9
quixote15 <santos2tiago@gmail.com>
2019-09-05 13:55:43 -0300

    Revert "Revert "Adiciona theads pra corrigir dissincronia""
    This reverts commit f79a378f333cf79fdab782133c893664cd43d057.

--- skills/nlu_processing.js
+++ skills/nlu_processing.js
@@ -6,41 +6,33 @@ module.exports = function(controller) {
   controller.middleware.receive.use(rasa.receive);
 
   controller.on("message_received", async function(bot, message) {
-    console.log("mensagem saas:", message);
+    //console.log("mensagem saas:", message);
 
     const { replies } = message;
-    //console.log(replies)
- 
-    for (var i = 0; i < replies.length; i++) {
-      const answer = replies[i];
-      console.log("text:", answer.text || answer.image);
 
-      const { text, image } = answer;
-      const reply = {
-        text,
-        replyWithTyping: true,
-        files: [
-          {
-            url: image,
-            image: !!image
-          }
-        ]
-      };
-      /* bot.reply(message, reply, function() {
-        console.log("done ", index);
-      });*/
-      await sendMessage(bot, message, reply);
-    }
+    bot.startConversation(message, function(err, convo) {
+      for (var i = 0; i < replies.length; i++) {
+        const answer = replies[i];
+      //  console.log("text:", answer.text || answer.image);
+      //  console.log("message:", message);
 
-    /*replies.forEach(async (answer, index) => {
-      
-    });*/
-    //bot.startConversation()
+        const { text, image } = answer;
+        const reply = {
+          text,
+          replyWithTyping: true,
+          files: [
+            {
+              url: image,
+              image: !!image
+            }
+          ]
+        };
+
+        convo.say(reply);
+
+      }
+    });
+    
   });
 
-  const sendMessage = async (bot, message, reply) => {
-    return new Promise(resolve =>
-      setTimeout(() => resolve(bot.reply(message, reply)), 1000)
-    );
-  };
 };
